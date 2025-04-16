fded0f2a8df74f1d64e982e5f673d77a31b273aa
w8385 <w8385@naver.com>
2025-04-16 17:54:45 +0000

    Update web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1763557833"
+    serviceWorkerVersion: "3867289251"
   }
 });
